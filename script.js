3634b6cc59e9498d9975169d0c0f2df07daf829f
HaiderRizvii <sha.rizvi883@gmail.com>
2021-07-01 19:01:24 +0500

    Canvas_Task
    This is  our week 2 assignment of Canvas.

---
+++ script.js
@@ -0,0 +1,88 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+const reader = new FileReader();
+const img = new Image();
+
+/* const cls = document
+  .getElementsByTagName('input')[1]
+  .getAttribute('class')
+  .split(' ')[1];
+
+console.log(cls); */
+
+const uploadImage = (e) => {
+  reader.onload = () => {
+    img.onload = () => {
+      canvas.width = img.width;
+      img.height = canvas.height;
+      ctx.drawImage(img, 0, 0);
+    };
+    img.src = reader.result;
+  };
+  reader.readAsDataURL(e.target.files[0]);
+};
+
+const imageLoader = document.getElementById('uploader');
+imageLoader.addEventListener('change', uploadImage);
+
+const writeOnHead = (e) => {
+  const textHead = document.getElementById('overText').value;
+  //const textFoot = document.getElementById('belowText').value;
+  //console.log(textFoot);
+  console.log(textHead);
+  ctx.font = 'italic 35px Arial';
+  ctx.textAlign = 'center';
+  ctx.textBaseline = 'middle';
+  ctx.fillStyle = headColor;
+  ctx.fillText(textHead, canvas.width / 2, 50);
+  //ctx.fillText(textFoot, canvas.width / 2, canvas.height - 20);
+};
+
+const writeOnFoot = (e) => {
+  const textFoot = document.getElementById('belowText').value;
+  console.log(textFoot);
+  ctx.font = 'italic 35px Arial';
+  ctx.textAlign = 'center';
+  ctx.textBaseline = 'middle';
+  ctx.fillStyle = footColor;
+  ctx.fillText(textFoot, canvas.width / 2, canvas.height - 20);
+};
+
+const writeOnImage = () => {
+  writeOnHead();
+  writeOnFoot();
+};
+
+document.getElementById('writeOnImage').addEventListener('click', writeOnImage);
+
+function download() {
+  const image = canvas.toDataURL();
+  const link = document.createElement('a');
+  link.href = image;
+  link.download = 'image.png';
+  link.click();
+}
+
+document.getElementById('download').addEventListener('click', download);
+
+let headColor = '#111';
+let footColor = '#111';
+
+const headColorPicker = (e) => {
+  headColor = document.getElementById('headColorPicker').value;
+  console.log(headColor);
+};
+
+const footColorPicker = (e) => {
+  footColor = document.getElementById('footColorPicker').value;
+  console.log(footColor);
+};
+
+headColor = document
+  .getElementById('headColorPicker')
+  .addEventListener('click', headColorPicker);
+
+footColor = document
+  .getElementById('footColorPicker')
+  .addEventListener('click', footColorPicker);
